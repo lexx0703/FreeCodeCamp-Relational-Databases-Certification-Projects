1850776d59f937a857e4648fba266c318ab3cdad
lexx0703 <90952668+lexx0703@users.noreply.github.com>
2026-06-10 18:32:38 +0300

    Add element information retrieval script
    This script retrieves and displays information about a chemical element based on its atomic number, symbol, or name.

---
+++ element.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+  exit
+fi
+
+if [[ $1 =~ ^[0-9]+$ ]]
+then
+  ELEMENT_DATA=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number = $1;")
+else
+  ELEMENT_DATA=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE symbol = '$1' OR name = '$1';")
+fi
+
+if [[ -z $ELEMENT_DATA ]]
+then
+  echo "I could not find that element in the database."
+  exit
+fi
+
+echo "$ELEMENT_DATA" | while IFS='|' read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING
+do
+  echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+done
